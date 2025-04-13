3ffd65fa8357b082b280858048d25d3bbb9b6b65
Manas Dongol <manasdongol27@gmail.com>
2025-04-13 10:33:13 +0545

    Bigyan ko page

--- TaskifyInc/src/main/java/com/jobpage/controller/Profilepage.java
+++ TaskifyInc/src/main/java/com/jobpage/controller/Profilepage.java
@@ -8,16 +8,16 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * Servlet implementation class login
+ * Servlet implementation class Profilepage
  */
-@WebServlet("/login")
-public class Login extends HttpServlet {
+@WebServlet(asyncSupported = true, urlPatterns = { "/profile" })
+public class Profilepage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Login() {
+    public Profilepage() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -27,7 +27,7 @@ public class Login extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		request.getRequestDispatcher("/WEB-INF/pages/login.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/pages/profile.jsp").forward(request, response);
 	}
 
 	/**
